1279df636d058364387c50b742221a9439c6ea6e
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-01-22 20:53:00 +0200

    feat: added login logic

--- systema/accounts/views.py
+++ systema/accounts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
 from .forms import LoginForm
 
-def login(request):
+def login_view(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
 
@@ -9,7 +11,13 @@ def login(request):
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
 
-            print(username, password)
+            user = authenticate(request, username=username, password=password)
+
+            if user:
+                login(request, user)
+                return HttpResponse('Logged In!')
+            else:
+                return HttpResponse('Incorrect credentials!')
 
     else:
         form = LoginForm()
