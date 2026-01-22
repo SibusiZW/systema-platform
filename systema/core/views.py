5c56e5a6f29fcb71498eb236065e0637e994fd5a
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-01-22 21:19:43 +0200

    feat: added navbar

--- systema/core/views.py
+++ systema/core/views.py
@@ -3,4 +3,4 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='/auth/login')
 def home(request):
-    return render(request, 'core/home.html')
+    return render(request, 'core/home.html', { 'user': request.user })
