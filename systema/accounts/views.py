557e1b88954c90bdeb3165c381370b86bc12b5e5
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-01-22 20:42:54 +0200

    feat: added login design

--- systema/accounts/views.py
+++ systema/accounts/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from .forms import LoginForm
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            print(username, password)
+
+    else:
+        form = LoginForm()
+
+    return render(request, 'login.html', { 'form': form })
