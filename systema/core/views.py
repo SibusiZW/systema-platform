1bd6e50ec13ca3d6e267841b898de09487b89bcf
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-01-22 21:22:02 +0200

    feat: added logout capability

--- systema/core/views.py
+++ systema/core/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='/auth/login')
 def home(request):
     return render(request, 'core/home.html', { 'user': request.user })
+
+@login_required(login_url='/auth/login')
+def signout(request):
+    logout(request)
+    return redirect('login')
