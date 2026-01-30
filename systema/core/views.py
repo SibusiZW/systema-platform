bf6d6967f650ae1cc2af6a9db349ca241b86b5d7
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-01-30 16:51:30 +0200

    feat: added stats page

--- systema/core/views.py
+++ systema/core/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
+from .models import Machine, Allocation, Student
 
 @login_required(login_url='/auth/login')
 def home(request):
-    return render(request, 'core/home.html', { 'user': request.user })
+    return render(request, 'core/home.html', { 'user': request.user, 'total_m_count': Machine.objects.count(), 'total_s_count': Student.objects.count(), 'good_pcs': Machine.objects.filter(condition="Good").count(), 'missing': Machine.objects.filter(condition="Missing").count() })
 
 @login_required(login_url='/auth/login')
 def signout(request):
